e3ee7fb7e2aee67bb9da0e5f3f42d15a8e8baab0
esquareddev <elldevspc@gmail.com>
2023-04-26 23:43:23 -0400

    Added HTML template for posts and dataset

--- app.py
+++ app.py
@@ -1,20 +1,46 @@
 #!/usr/bin/python3
 from flask import Flask, render_template
+import dadjokes
 
 app = Flask(__name__)
 
+# THIS IS A LIIS OF DICT PAIRS WITH JOKES
+REDDIT_JOKES = dadjokes.ALL_JOKES
+
 POSTS = [
   {
-    "id": 1,
+    "id": 0,
+    "post_date": "Today",
+    "category": "Tech",
     "title":"How To Automate With Python",
-    "description":"some randome text for the description"
+    "description":"some randome text for the description",
+    "subreddit":"dadjokes"
+
+  },
+  {
+    "id": 1,
+    "post_date": "Today",
+    "category": "Tech",
+    "title":"10 Best Practices for Implementing DevOps in Your Organization",
+    "description":"This post will provide actionable tips on how to successfully implement DevOps in your organization, including tips for collaboration, automation, and monitoring",
+    "subreddit":"dadjokes"
+
+  },
+  {
+    "id": 2,
+    "post_date": "Today",
+    "category": "Tech",
+    "title":"5 Common DevOps Mistakes and How to Avoid Them",
+    "description":"This post will identify common mistakes made by organizations when implementing DevOps and provide tips on how to avoid them, including tips for culture, automation, and monitoring.",
+    "subreddit":"dadjokes"
+    
   }
 ]
 
 # register app
 @app.route("/")
 def show_page():
-  return render_template('index.html', posts=POSTS)
+  return render_template('index.html', posts=POSTS, jokes=REDDIT_JOKES)
 
 
 if __name__ == "__main__":
